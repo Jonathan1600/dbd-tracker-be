1993d10888e188961f0c85cbe55e2bfdab79f6ec
Jonathan1600 <jonathancalderonsilberman@gmail.com>
2021-08-31 16:26:13 -0700

    database migrations and data seeded

--- database/seeds/02-test-subjects.js
+++ database/seeds/02-test-subjects.js
@@ -2,7 +2,22 @@
 exports.seed = function (knex) {
   const users = [
     {
-      username: "Trapper"
+      username: "Trapper",
+      total_hours: 15.5,
+      acct_created: '2015-12-25',
+      total_bloodpoints: 3654180,
+      ult_rare_offerings: 4,
+      mystery_boxes_opened: 89,
+      survivor_rank: 4,
+      survivor_perfect_games: 14,
+      ult_rare_addons_surv: 0,
+      survivor_full_loadout: 29,
+      generators_repaired: 554,
+      escaped_alive: 0,
+      killer_rank: 1,
+      killer_full_loadout: 0,
+      survivors_sacrificed: 80,
+      hatches_closed: 30
     }
   ];
 
